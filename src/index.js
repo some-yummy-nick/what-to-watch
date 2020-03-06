fa2ef2da6210f88d7e78ce42801c5fff69e42f38
Marat <yeffasol@mail.ru>
2020-03-06 22:17:47 +0300

    Add first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {App} from "./components/App/App.jsx";
+
+const init = () => {
+  ReactDOM.render(
+    <App/>,
+    document.getElementById(`root`)
+  );
+};
+
+init();
